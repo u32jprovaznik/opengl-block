a7ef1452fc0a787805cfae1e035ee2a078586243
u32jprovaznik <jprovaznik@proton.me>
2023-05-12 22:30:31 +0200

    Creating a GLFW window

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,4 +1,75 @@
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
+
+// Constants
+const unsigned int WINDOW_WIDTH  = 800;
+const unsigned int WINDOW_HEIGHT = 600;
+const char*        WINDOW_TITLE  = "GLFW";
+
+// Callbacks
+void framebufferSizeCallback(GLFWwindow* window, GLint width, GLint height);
+
 int main()
 {
+  // Initializing GLFW
+  glfwInit();
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+  // Creating the Window
+  GLFWwindow* window = glfwCreateWindow(
+    WINDOW_WIDTH,
+    WINDOW_HEIGHT,
+    WINDOW_TITLE,
+    NULL,
+    NULL
+  );
+  if (window == NULL)
+  {
+    std::cerr << "Failed to initialize a GLFW Window!" << std::endl;
+    glfwTerminate();
+    return -1;
+  }
+  glfwMakeContextCurrent(window);
+  glViewport(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT);
+
+  // Initializing GLEW
+  GLenum err = glewInit();
+  if (GLEW_OK != err)
+  {
+    std::cerr << "Failed to initialze GLEW!" << std::endl;
+    std::cerr << "Error: " << glewGetErrorString(err) << std::endl;
+    glfwTerminate();
+    return -1;
+  }
+  std::cout << "GLEW: " << glewGetString(GLEW_VERSION) << std::endl;
+
+  // Clear Color
+  GLclampf red   = 0.0f;
+  GLclampf green = 0.3f;
+  GLclampf blue  = 0.3f;
+  GLclampf alpha = 1.0f;
+  glClearColor(red, green, blue, alpha);
+
+  // Callbacks
+  glfwSetFramebufferSizeCallback(window, framebufferSizeCallback);
+
+  // Main Loop
+  while (!glfwWindowShouldClose(window))
+  {
+    glfwPollEvents();
+    glClear(GL_COLOR_BUFFER_BIT);
+    glfwSwapBuffers(window);
+  }
+
+  // Terminating the Program
+  glfwTerminate();
   return 0;
+}
+
+void framebufferSizeCallback(GLFWwindow* window, GLint width, GLint height)
+{
+  glViewport(0, 0, width, height);
 }
